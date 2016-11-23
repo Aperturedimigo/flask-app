916c0be0202c54b91c985a9d0cd3c3f4c097daa6
CatNote@LLr <CatNote1@gmail.com>
2016-11-24 03:23:36 +0900

    Update Aperture.js

--- static/Aperture.js
+++ static/Aperture.js
@@ -9,4 +9,52 @@ $(window).bind('scroll', function() {
     document.getElementById('sublogo').classList.remove('visible');
     document.getElementById('sublogo').classList.add('hidden');
   }
+  if($(window).scrollTop() < 140) {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      document.getElementById(N).classList.remove('bold');
+    }
+  }
+  else if($(window).scrollTop() < 900) {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      if(i == 1) document.getElementById(N).classList.add('bold');
+      else document.getElementById(N).classList.remove('bold');
+    }
+  }
+  else if($(window).scrollTop() < 1700) {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      if(i == 2) document.getElementById(N).classList.add('bold');
+      else document.getElementById(N).classList.remove('bold');
+    }
+  }
+  else if($(window).scrollTop() < 2720) {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      if(i == 3) document.getElementById(N).classList.add('bold');
+      else document.getElementById(N).classList.remove('bold');
+    }
+  }
+  else if($(window).scrollTop() < 3280) {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      if(i == 4) document.getElementById(N).classList.add('bold');
+      else document.getElementById(N).classList.remove('bold');
+    }
+  }
+  else if($(window).scrollTop() < 3690) {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      if(i == 5) document.getElementById(N).classList.add('bold');
+      else document.getElementById(N).classList.remove('bold');
+    }
+  }
+  else {
+    for(var i = 1; i <= 6; i++) {
+      var N = 't' + i;
+      if(i == 6) document.getElementById(N).classList.add('bold');
+      else document.getElementById(N).classList.remove('bold');
+    }
+  }
 });
