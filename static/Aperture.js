7378bc93458f61dc971eae2cdcedb0c3c38c5dc5
CatNote@LLr <CatNote1@gmail.com>
2016-11-24 04:02:51 +0900

    Update Aperture.js

--- static/Aperture.js
+++ static/Aperture.js
@@ -12,49 +12,49 @@ $(window).bind('scroll', function() {
   if($(window).scrollTop() < 140) {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      document.getElementById(N).classList.remove('bold');
+      document.getElementById(N).classList.remove('underline');
     }
   }
   else if($(window).scrollTop() < 900) {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      if(i == 1) document.getElementById(N).classList.add('bold');
-      else document.getElementById(N).classList.remove('bold');
+      if(i == 1) document.getElementById(N).classList.add('underline');
+      else document.getElementById(N).classList.remove('underline');
     }
   }
   else if($(window).scrollTop() < 1700) {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      if(i == 2) document.getElementById(N).classList.add('bold');
-      else document.getElementById(N).classList.remove('bold');
+      if(i == 2) document.getElementById(N).classList.add('underline');
+      else document.getElementById(N).classList.remove('underline');
     }
   }
   else if($(window).scrollTop() < 2720) {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      if(i == 3) document.getElementById(N).classList.add('bold');
-      else document.getElementById(N).classList.remove('bold');
+      if(i == 3) document.getElementById(N).classList.add('underline');
+      else document.getElementById(N).classList.remove('underline');
     }
   }
   else if($(window).scrollTop() < 3280) {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      if(i == 4) document.getElementById(N).classList.add('bold');
-      else document.getElementById(N).classList.remove('bold');
+      if(i == 4) document.getElementById(N).classList.add('underline');
+      else document.getElementById(N).classList.remove('underline');
     }
   }
   else if($(window).scrollTop() < 3690) {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      if(i == 5) document.getElementById(N).classList.add('bold');
-      else document.getElementById(N).classList.remove('bold');
+      if(i == 5) document.getElementById(N).classList.add('underline');
+      else document.getElementById(N).classList.remove('underline');
     }
   }
   else {
     for(var i = 1; i <= 6; i++) {
       var N = 't' + i;
-      if(i == 6) document.getElementById(N).classList.add('bold');
-      else document.getElementById(N).classList.remove('bold');
+      if(i == 6) document.getElementById(N).classList.add('underline');
+      else document.getElementById(N).classList.remove('underline');
     }
   }
 });
